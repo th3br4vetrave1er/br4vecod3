5fba251de0bb9f4b38cee2f5ce679efb7e8c7c85
br4vetrave1er <tazdingodan@gmail.com>
2024-06-06 20:27:24 +0300

    1

--- punching_knuckles.py
+++ punching_knuckles.py
@@ -1,1 +1,21 @@
+# prompt = "Dan is bad"
+# print(prompt)
+# print(prompt.replace("bad", "good"))
+# print(prompt.title())
+# print(len(prompt))
+# prompt += "                                       "
+# print(len(prompt))
+# prompt = "                         " + prompt
+# print(len(prompt))
+# print(prompt.strip())
+# print(prompt.lstrip())
+# print(prompt.rstrip())
+
+# title = "menu".upper()
+# print(title.center(20, "="))
+# print("Coffee".ljust(16, ".") + "1$".rjust(4))
+# print("Muffin".ljust(16, ".") + "30$".rjust(4))
+# print("Dick".ljust(16, ".") + "2$".rjust(4))
+# print("Ass".ljust(16, ".") + "20$".rjust(4))
+# print(isinstance(title, str))
 
