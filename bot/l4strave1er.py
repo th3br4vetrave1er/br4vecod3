44647eb45ee06cccbfbba6895b4c8356f5d3eba3
br4vetrave1er <tazdingodan@gmail.com>
2024-06-10 20:42:19 +0300

    1

--- bot/l4strave1er.py
+++ bot/l4strave1er.py
@@ -1,8 +1,21 @@
+import os
+from dotenv import load_dotenv
 import telebot
-bot = telebot.TeleBot(token="7312344875:AAEvTxC-AEy9u4bc1yua5mqR-g5FePZ2j3s")
 
+# Загружаем переменные из .env файла
+load_dotenv()
 
-@bot.message_handler(commands=["start"])
-def start_message(message):
+# Получаем токен из переменной окружения
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
+# Проверяем, что токен загружен правильно
+if not TOKEN:
+    raise ValueError("No TOKEN found in environment variables")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
     bot.send_message(message.chat.id, "Никто тебя не любит. Все тебя ненавидят. Они проиграют. Улыбнись, уёбан.")
-bot.infinity_polling
+
+bot.polling(none_stop=True)
