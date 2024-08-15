c136a6c44a6262dca1bc5585c2082998c0034907
bravetrave1er <tazdingodan@gmail.com>
2024-08-15 14:27:18 +0300

    1

--- punching_knuckles.py
+++ punching_knuckles.py
@@ -1,3 +1,9 @@
-z = "1 2 3 4 5 6"
-print(list(map(lambda x: int(x), z)))
-
+apples = 3.45
+quantity = 0
+is_in_store = None
+if quantity > 0:
+    is_in_store = True
+else:
+    is_in_store = False
+    
+print(is_in_store)
