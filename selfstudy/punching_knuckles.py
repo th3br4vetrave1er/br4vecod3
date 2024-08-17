fedf5b9f2d4064407ba00d65fddc395325dd7100
bravetrave1er <tazdingodan@gmail.com>
2024-08-17 15:52:44 +0300

    0

--- selfstudy/punching_knuckles.py
+++ selfstudy/punching_knuckles.py
@@ -1,9 +1,11 @@
-apples = 3.45
-quantity = 0
-is_in_store = None
-if quantity > 0:
-    is_in_store = True
-else:
-    is_in_store = False
-    
-print(is_in_store)
+sales_w1 = [7,3,42,19,15,35,9]
+sales_w2 = [12,4,26,10,7,28]
+sales = []
+day = int(input("Enter another day to week 2: "))
+sales_w2.append(day)
+sales = sales_w1 + sales_w2
+worst_day = min(sales) * 1.5
+best_day = max(sales) * 1.5
+print(f"On the best day you have earned {best_day}")
+print(f"On the worst day you have earned {worst_day}")
+print(f"On the first week you earned {sales_w1}, and on the second week you earned {sales_w2}, in total its {worst_day + best_day} adenas.")
