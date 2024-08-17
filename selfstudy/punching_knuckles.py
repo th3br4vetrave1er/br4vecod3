a5b656480ac3e84ff55ab344f706d2a2653259bd
bravetrave1er <tazdingodan@gmail.com>
2024-08-17 20:40:48 +0300

    1

--- selfstudy/punching_knuckles.py
+++ selfstudy/punching_knuckles.py
@@ -1,11 +1,14 @@
-sales_w1 = [7,3,42,19,15,35,9]
-sales_w2 = [12,4,26,10,7,28]
-sales = []
-day = int(input("Enter another day to week 2: "))
-sales_w2.append(day)
-sales = sales_w1 + sales_w2
-worst_day = min(sales) * 1.5
-best_day = max(sales) * 1.5
-print(f"On the best day you have earned {best_day}")
-print(f"On the worst day you have earned {worst_day}")
-print(f"On the first week you earned {sales_w1}, and on the second week you earned {sales_w2}, in total its {worst_day + best_day} adenas.")
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+friends_list = ['Exercise: fill me with names']
+
+# From the list above fill a list(friends_list) properly
+# with the names of all the friends. One per "slot"
+# you may need to run same command several times
+# use print() statements to work your way through the exercise
+friends_list.pop()
+csv = csv.replace(",", " ")
+csv = csv.replace(":", " ")
+csv = csv.replace(";", " ")
+csv = csv.split()
+print(csv)
+print(friends_list)
