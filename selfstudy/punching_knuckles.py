89ef376fec0bfb793ba8cc6aa3ec723275c23fac
bravetrave1er <tazdingodan@gmail.com>
2024-08-18 12:55:58 +0300

    1

--- selfstudy/punching_knuckles.py
+++ selfstudy/punching_knuckles.py
@@ -1,14 +1,19 @@
-csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
-friends_list = ['Exercise: fill me with names']
+def greeting(name, age=28, color="red"):
+    #Greets user with 'name' from 'input box' and 'age', if available, default age is used
+    # print('Hello '  +  name.title() + ', you are ' + str(age + 1) +'!')
+    print(f'Hello {name.title()}, you are {age + 1}!')
+    print(f"We heard you like {color.lower()} color!")
 
-# From the list above fill a list(friends_list) properly
-# with the names of all the friends. One per "slot"
-# you may need to run same command several times
-# use print() statements to work your way through the exercise
-friends_list.pop()
-csv = csv.replace(",", " ")
-csv = csv.replace(":", " ")
-csv = csv.replace(";", " ")
-csv = csv.split()
-print(csv)
-print(friends_list)
+
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+color = input("What color do you like: ")
+greeting(name, int(age), color)
+# 1. Add new print statement - on a new line
+#    which says 'We hear you like the color xxx! xxx is a string with color 
+# 2. extend the function with another  input parameter 'color', that defaults to 'red'
+# 3. Capture the color via an input box as variable:color 
+# 4. Change the 'You are xx!' text to say 'you will be xx+1 years old next birthday 
+#  adding 1 to the age
+# 5. Capitalize first letter of the 'name', and rest are small caps 
+# 6. Favorite color should be in lowercase 
