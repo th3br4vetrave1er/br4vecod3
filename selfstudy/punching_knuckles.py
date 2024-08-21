f4feff754bdbf2089d7fd02837d1e5920d777334
bravetrave1er <tazdingodan@gmail.com>
2024-08-21 19:52:23 +0300

    1

--- selfstudy/punching_knuckles.py
+++ selfstudy/punching_knuckles.py
@@ -1,19 +1,11 @@
-def greeting(name, age=28, color="red"):
-    #Greets user with 'name' from 'input box' and 'age', if available, default age is used
-    # print('Hello '  +  name.title() + ', you are ' + str(age + 1) +'!')
-    print(f'Hello {name.title()}, you are {age + 1}!')
-    print(f"We heard you like {color.lower()} color!")
+def func(example):
+    example.sort()
+    if (example[0] ** 2) + (example[1] ** 2) == example[2] ** 2:
+        return True
+    else:
+        return False
+        
+        
+test = [100, 3, 65]
 
-
-name = input('Enter your name: ')
-age = input('Enter your age: ')
-color = input("What color do you like: ")
-greeting(name, int(age), color)
-# 1. Add new print statement - on a new line
-#    which says 'We hear you like the color xxx! xxx is a string with color 
-# 2. extend the function with another  input parameter 'color', that defaults to 'red'
-# 3. Capture the color via an input box as variable:color 
-# 4. Change the 'You are xx!' text to say 'you will be xx+1 years old next birthday 
-#  adding 1 to the age
-# 5. Capitalize first letter of the 'name', and rest are small caps 
-# 6. Favorite color should be in lowercase 
+print(func(test))
